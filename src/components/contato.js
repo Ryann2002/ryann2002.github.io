e422b1f60152bb0a145c2d09ef250b540f956974
Ryan Ferreira <ryanferreira135@gmail.com>
2022-06-12 17:30:33 -0300

    first commit

---
+++ src/components/contato.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+const Contato = () => {
+    return (
+        <section id="hero" className='-mb-2'>
+        </section>
+      );
+}
+ 
+export default Contato;
---
+++ src/components/title.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+
+const Title = ({children}) => {
+    return (<h2 className={"font-[kanit] text-4xl font-bold my-[40px] text-[#3176E0]"} >{children}</h2>)
+}
+
+export default Title;
